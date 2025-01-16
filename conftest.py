6a94de02befd41709556bfc9e36d51408f5a7d21
Julian <julianemran@gmail.com>
2025-01-16 15:35:26 +0200

    Add virtual display to run the tests in GUI mode

--- conftest.py
+++ conftest.py
@@ -1,13 +1,12 @@
-import time
+import sys
 import pytest
-import random
+from pyvirtualdisplay import Display
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 def get_chrome_browser_options():
-    """Configure Chrome options for legitimate automated testing"""
+    """Configure Chrome options for legitimate automated testing."""
     options = Options()
 
     # Basic required options
@@ -19,10 +18,19 @@ def get_chrome_browser_options():
     options.add_argument('--disable-infobars')
     return options
 
+
 @pytest.fixture(scope="class")
 def browser():
+    # Start a virtual display
+    if sys.platform == "linux":
+        display = Display(visible=0, size=(1920, 1080))
+        display.start()
+
+    # Initialize the browser with the specified options
     new_browser = uc.Chrome(use_subprocess=True, options=get_chrome_browser_options())
     new_browser.implicitly_wait(3)
     yield new_browser
+    # Ensure the browser and display are cleaned up
     new_browser.quit()
-
+    if sys.platform == "linux":
+        display.stop()
